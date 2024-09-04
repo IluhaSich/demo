94375344ab2da86058a8512045513405e83891f9
Ilya <ilyasichev2015@gmail.com>
2024-09-04 22:19:19 +0300

    add base objects for taco cloud; add html pages (home,design), explore @ModelAttribute annotation

---
+++ src/main/java/com/example/demo/tacos/Taco.java
@@ -0,0 +1,11 @@
+package com.example.demo.tacos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
